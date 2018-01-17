2ae84440c909a1642c014f4dff4506e000c65d66
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-17 14:12:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
